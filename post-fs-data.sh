23fe21e0dd6ff48494a60f44df0a900bea986631
iamlazy123 <71618647+iamlazy123@users.noreply.github.com>
2020-10-01 07:39:47 +0000

    updated to 7.2

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -9,122 +9,65 @@ SIZE=$(((RAM * FRACTION / 100) * 1048576))
 swapoff /dev/block/zram0 > /dev/null 2>&1
 echo '1' > /sys/block/zram0/reset
 echo '0' > /sys/block/zram0/disksize
-echo $(($CPUS - 2 )) > /sys/block/zram0/max_comp_streams
+echo $CPUS > /sys/block/zram0/max_comp_streams
 echo $SIZE > /sys/block/zram0/disksize
 mkswap /dev/block/zram0 > /dev/null 2>&1
 swapon /dev/block/zram0 > /dev/null 2>&1
-# avoid conflicts
+#
 setprop sys.use_fifo_ui 1
+setprop windowsmgr.max_events_per_sec 300
 setprop dalvik.vm.dex2oat-swap true
-setprop debug.hwui.renderer skiagl
-setprop debug.egl.swapinterval -60
-setprop vendor.debug.egl.swapinterval -60
-setprop debug.sf.latch_unsignaled 1
-setprop wifi.supplicant_scan_interval 200
 setprop dalvik.vm.gc.overwritefree true
-setprop debug.egl.buffcount 3
+setprop wifi.supplicant_scan_interval 360
 setprop ADRENO_PROFILER_ENABLE_OPENCL 1
-setprop ADRENO_PROFILER_ENABLE_BLOCKING 1
-setprop dalvik.vm.dex2oat-filter speed
-setprop dalvik.vm.image-dex2oat-filter --no-watch-dog
-setprop dalvik.vm.dexopt-data-only 1
+#
+setprop dalvik.vm.boot-dex2oat-threads $CPUS
+setprop ro.sys.fw.dex2oat_thread_count $CPUS
+setprop dalvik.vm.checkjni false
 setprop dalvik.vm.verify-bytecode false
-setprop dalvik.vm.execution-mode intjit
-setprop dalvik.vm.stack-trace-file /data/anr/traces.txt
-setprop dalvik.vm.jmiopts forcecopy
-setprop dalvik.vm.check-dex-sum false
-setprop dalvik.vm.deadlock-predict off
-setprop dalvik.vm.dex2oat-minidebuginfo false
-setprop dalvik.vm.usejit true
-setprop dalvik.vm.dex2oat-flags --no-watch-dog
-setprop dalvik.vm.dexopt.secondary true
-setprop pm.dexopt.first-boot speed
-setprop pm.dexopt.boot speed
-setprop pm.dexopt.install quicken
-setprop pm.dexopt.inactive speed
-setprop persist.sys.dalvik.multithread true
-setprop persist.sys.dalvik.hyperthreading true
-setprop pm.dexopt.bg-dexopt speed
-setprop pm.dexopt.shared speed
-setprop dalvik.vm.boot-dex2oat-threads $(($CPUS - 2 ))
-setprop dalvik.vm.dex2oat-threads $(($CPUS - 2))
-setprop dalvik.vm.image-dex2oat-threads $(($CPUS - 2))
-setprop ro.sys.fw.dex2oat_thread_count $(($CPUS - 2 ))
-#Dozeparameters
-setprop pm.sleep_mode 1
-#test
-setprop ro.sys.fw.bg_apps_limit 128
-#hwui
+setprop dalvik.vm.execution-mode int:jit
+setprop dalvik.vm.lockprof.threshold 500
+setprop ro.malloc.impl jemalloc
+#
 setprop ro.hwui.patch_cache_size 384
 setprop ro.hwui.drop_shadow_cache_size 18
 setprop ro.hwui.layer_cache_size 90
 setprop ro.hwui.path_cache_size 36
 setprop ro.hwui.r_buffer_cache_size 18
 setprop ro.hwui.texture_cache_size 183
-
-#touch
-setprop touch.pressure.scale 0.0001
-setprop touch.size.calibration diameter
-setprop touch.size.scale 1
-setprop touch.size.bias 0
-setprop touch.size.isSummed 0
-setprop touch.orientation.calibration none
-setprop touch.distance.calibration none
-setprop touch.distance.scale 0
-setprop touch.coverage.calibration box
-setprop touch.gestureMode spots
-
-#Gametweaks
-setprop debug.gr.swapinterval 0
-setprop windowsmgr.max_events_per_sec 300
-#Graphics
-setprop debug.gr.numframebuffers 3
-#SurfaceFlinger
 setprop ro.surface_flinger.max_frame_buffer_acquired_buffers 3
-#fasterapplaunching
-setprop persist.device_config.runtime_native.usap_pool_enabled true
-
-#Perf
-setprop ro.vendor.gt_library libqti-gt.so
-setprop ro.vendor.at_library libqti-at.so
-setprop ro.vendor.extension_library libqti-perfd-client.so
-setprop ro.vendor.scroll.preobtain.enable false
-
-#Graphics
-setprop debug.sf.gpu_comp_tiling 0
-setprop debug.sf.recomputecrop 0
-setprop debug.cpurend.vsync false
-setprop debug.enabletr true
-setprop debug.performance.tuning 1
-setprop ro.sf.compbypass.enable 0
-setprop debug.overlayui.enable 1
-setprop ro.product.gpu.driver 1
+setprop debug.gr.numframebuffers 3
+setprop debug.qctwa.preservebuf 1
+setprop dev.pm.dyn_samplingrate 1
+setprop debug.sf.disable_backpressure 1
 setprop debug.composition.type dyn
+setprop persist.sys.composition.type dyn
 setprop debug.egl.hw 1
 setprop debug.enabletr true
-setprop debug.hwui.render_dirty_regions false
-setprop debug.sf.disable_backpressure 1
-setprop persist.sys.composition.type dyn
+setprop debug.overlayui.enable 1
+setprop debug.performance.tuning 1
+setprop ro.sf.compbypass.enable 0
 setprop libc.debug.malloc 0
+setprop debug.gr.swapinterval -60
+setprop debug.hwui.renderer skiagl
+setprop debug.egl.swapinterval -60
+setprop vendor.debug.egl.swapinterval -60
+setprop debug.sf.latch_unsignaled 1
+setprop debug.egl.buffcount 4
+setprop ro.sf.disable_triple_buffer 0
+setprop net.tcp.2g_init_rwnd 10
+setprop net.tcp.default_init_rwnd 60
+setprop net.tethering.noprovisioning true
 setprop hwui.disable_vsync true
+setprop debug.sf.recomputecrop 0
+setprop debug.force_rtl false
+setprop debug.cpurend.vsync false
 setprop hwui.render_dirty_regions false
-
-#SurfaceFlinger
-setprop ro.surface_flinger.support_kernel_idle_timer true
-setprop ro.surface_flinger.set_display_power_timer_ms 10000
-setprop ro.surface_flinger.set_touch_timer_ms 5000
-setprop ro.surface_flinger.set_idle_timer_ms 9000
-
-#eglgetdefaultdisplay
-setprop ro.zygote.disable_gl_preload true
-
-#perf
-setprop vendor.perf.gestureflingboost.enable true
-setprop vendor.iop.enable_iop 0
-setprop vendor.iop.enable_uxe 0
-setprop vendor.perf.iop_v3.enable 0
-setprop vendor.perf.iop_v3.enable.debug 0
-setprop vendor.enable.prefetch 0
-setprop vendor.iop.enable_prefetch_ofr 0
+#
+mount -o remount,nosuid,nodev,noatime,nodiratime,data=writeback -t auto /;
+mount -o remount,nosuid,nodev,noatime,nodiratime -t auto /proc;
+mount -o remount,nosuid,nodev,noatime,nodiratime,data=writeback -t auto /sys;
+mount -o remount,nosuid,nodev,noatime,nodiratime,barrier=0,noauto_da_alloc,discard -t auto /data;
+mount -o remount,nodev,noatime,nodiratime,barrier=0,noauto_da_alloc,discard -t auto /dev/root;
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
--- service.sh
+++ service.sh
@@ -1,13 +1,23 @@
 #!/system/bin/sh
-# Written by Draco (tytydraco @ GitHub)
-
-# Wait for boot to finish completely
-while [[ `getprop sys.boot_completed` -ne 1 ]] && [[ ! -d "/sdcard" ]]
-do
+wait_until_login()
+{
+    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
+    while [[ `getprop sys.boot_completed` -ne 1 ]] && [[ ! -d "/sdcard" ]]
+	do
        sleep 1
-done
+	done
 
-# Sleep an additional 60s to ensure init is finished
-sleep 120
+    local test_file="/sdcard/.PERMISSION_TEST"
+    touch "$test_file"
+    while [ ! -f "$test_file" ]; do
+        touch "$test_file"
+        sleep 1
+    done
+    rm "$test_file"
+}
+wait_until_login
+#
+sleep 60
 # Setup tweaks
 lazy
+
