9d625d87792f9b414000e50111aa4689e4475b35
root <alexfinhart@gmail.com>
2021-03-16 16:41:18 +0800

    pandora: make setup faster

--- service.sh
+++ service.sh
@@ -2,13 +2,13 @@
 
 wait_until_login()
 {
-    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
-    while [[ `getprop sys.boot_completed` -ne 1 ]] && [[ ! -d "/sdcard" ]]
-	do
-       sleep 1
-	done
+     # in case of /data encryption is disabled
+    while [ "$(getprop sys.boot_completed)" != "1" ]; do
+        sleep 1
+    done
 
-    local test_file="/sdcard/.PERMISSION_TEST"
+    # we doesn't have the permission to rw "/sdcard" before the user unlocks the screen
+    local test_file="/sdcard/Android/.PERMISSION_TEST"
     touch "$test_file"
     while [ ! -f "$test_file" ]; do
         touch "$test_file"
@@ -17,14 +17,8 @@ wait_until_login()
     rm "$test_file"
 }
 wait_until_login
-
-echo "Please wait for 30 seconds, Pandora is waiting for your vendor scripts to finish executing." > /sdcard/android/pandora.log
-
 rm -rf /sdcard/android/lazy.log
 rm -rf /sdcard/android/swift.log
-
-sleep 30
-
 rm -rf /sdcard/android/pandora.log
 # Setup tweaks
 pandora
